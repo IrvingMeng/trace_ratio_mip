6c14722973c00c2be64a3ffa487458dacfaa05e4
IrvingMeng <qiangm@uw.edu>
2017-08-02 08:00:29 -0700

    feat: add plot, showing features on an image

--- exp_ORL_part2.py
+++ exp_ORL_part2.py
@@ -2,111 +2,103 @@
 # Experiment for ORL dataset
 # ----------------------------------------------------------------------
 # This code will include these parts: 
-# [DONE] 2. Use 1NN to give the test accuracy, run 20 times
-#             a. naive approach
-#             b. subset approach
-#             c. MIO approach
+# [TODO] Compare the select  features when m=100, shown in a picture
+# [TODO] Report how many features and constraints are added
+# [TODO] With the increasing of m, report
+#           1. the running time v.s. without using the monotonic property
+#           2. Running time v.s. without using our framework at all
+#           3. number of eliminated features
 # ----------------------------------------------------------------------
-
 import numpy as np
-from random import shuffle
 import heapq
 import cplex
 from matplotlib import pyplot as plt
-import seaborn as sns
-from sklearn.neighbors import KNeighborsClassifier
+import matplotlib.image as mpimg
+from sklearn.datasets import fetch_olivetti_faces
+
 
 # import functions
-from data_orl_create import data_orl_random_split, data_orl_add_constraints
+from data_orl_create import data_orl_add_constraints
 from function_benchmark import nie_alg, naive_trace_ratio
 from function_mip import setupproblem_basic, eliminate_features
-
-
-
-def fit_1NN(train_x, train_y, test_x, test_y, features):
-    tmp_train_x = train_x[features,:].T
-    tmp_test_x = test_x[features, :].T
-    neigh = KNeighborsClassifier(n_neighbors=1)
-    predict_y = neigh.fit(tmp_train_x, train_y) 
-    predict_y = np.array(predict_y)
-    test_y = np.array(test_y)
-    error_num =  np.where(test_y - neigh.predict(tmp_test_x)!=0)[0].shape[0]
-    return error_num
-
-
-# These are the control parameters
-# ==============================
-loop_num = 20
-threshold = 0.98
-m_list = xrange(10,110,10)
-# m_list = [10,20,30]
-# ==============================
-
-# things to record
-# ==============================
-run_time = []
-error_naive = []
-error_nie = []
-error_mio = []
-# ==============================
 
 d = 4096
 var_p = ['p'+str(i) for i in xrange(1,d+1)]
 var_l = ['l'+str(i) for i in xrange(1,d+1)]
 var_lambda = ['lambda']
 
-for loop_i in range(loop_num):
-    print 'loop ' + str(loop_i) + '...'
-    mio_monot = 10000 # the bound of monotonic property, set to largest each time
-    train_x, train_y, test_x, test_y, dig_B, dig_E = data_orl_random_split()
-    cons_var, cons_coef, cons_rhs, ignore_list =  data_orl_add_constraints(threshold)
-    error_m_naive = []
-    error_m_nie = []
-    error_m_mio = []
-    for m in m_list:
-        # naive approach
-        features_naive, naive_ratio = naive_trace_ratio(dig_B, dig_E, m)
-        error_m_naive.append(fit_1NN(train_x, train_y, test_x, test_y, features_naive))
+_data = np.load('./data/orl_face/orl_data.npz')
+dig_B = _data['dig_B']
+dig_E = _data['dig_E']
+cons_var, cons_coef, cons_rhs, ignore_list =  data_orl_add_constraints(0.98)
 
-        # nie approach
-        features_nie, nie_ratio = nie_alg(dig_B,dig_E, m)
-        error_m_nie.append(fit_1NN(train_x, train_y, test_x, test_y, features_nie))
+# ----------
+print len(cons_rhs) # of new constraints
+print len(ignore_list) # of involved featues
 
-        # the mio approach
-        U_m = min([max(dig_B/dig_E), nie_ratio, sum(heapq.nlargest(m, dig_B))/sum(heapq.nsmallest(m, dig_E)), mio_monot])
-        L_m = max([min(dig_B/dig_E), sum(heapq.nsmallest(m, dig_B))/sum(heapq.nlargest(m, dig_E))])
-        c = cplex.Cplex()
-        setupproblem_basic(c, m, d, dig_B, dig_E, U_m, L_m, cons_var, cons_coef, cons_rhs)
-        print 'eliminate '+ str(eliminate_features(c, d, dig_B, dig_E, U_m, ignore_list)) + '  variables'
-        time = c.get_time()
-        c.solve()
-        run_time.append(c.get_time() - time)
-        sol = c.solution
-        mio_monot = sol.get_objective_value()
 
-        features_mio = np.where(np.array(sol.get_values(var_p))>=0.5)[0]
-        error_m_mio.append(fit_1NN(train_x, train_y, test_x, test_y, features_mio))
-    error_naive.append(error_m_naive)
-    error_nie.append(error_m_nie)
-    error_mio.append(error_m_mio)
+# ------------------------------
+# this part produce a image to show the selected features
+# can consider change threhsold
+m = 200
+features_naive, naive_ratio = naive_trace_ratio(dig_B, dig_E, m)
+features_nie, nie_ratio = nie_alg(dig_B,dig_E, m)
+mio_monot = 3 # the bound of monotonic property, set to largest each time    
+# the mio approach
+U_m = min([max(dig_B/dig_E), nie_ratio, sum(heapq.nlargest(m, dig_B))/sum(heapq.nsmallest(m, dig_E)), mio_monot])
+L_m = max([min(dig_B/dig_E), sum(heapq.nsmallest(m, dig_B))/sum(heapq.nlargest(m, dig_E))])
+c = cplex.Cplex()
+setupproblem_basic(c, m, d, dig_B, dig_E, U_m, L_m, cons_var, cons_coef, cons_rhs)
+print 'eliminate '+ str(eliminate_features(c, d, dig_B, dig_E, U_m, ignore_list)) + '  variables'
+c.solve()
+sol = c.solution
+features_mio = np.where(np.array(sol.get_values(var_p))>=0.5)[0]
+
+
+# >>> features_mio
+# array([   9,   11,   14,   18,   22,   26,   30,   35,   38,   49,   51,
+#          53,   55,   56,   72,   74,   76,   80,   96,  137,  142,  146,
+#         149,  153,  157,  163,  165,  167,  181,  183,  200,  203,  205,
+#         207,  210,  213,  216,  219,  223,  225,  228,  230,  249,  265,
+#         266,  268,  272,  277,  280,  282,  284,  286,  288,  290,  292,
+#         344,  346,  348,  350,  352,  354,  356,  412,  414,  416,  418,
+#         419,  481,  522,  523,  524,  544,  545,  546,  585,  586,  587,
+#         588,  589,  844,  845,  846,  848,  849,  907,  908,  909, 1293,
+#        1612, 1614, 1674, 1675, 1677, 1740, 2060, 2187, 2250, 2377, 2440,
+#        2505])
+# >>> features_naive
+# array([148, 150, 149, 151,  19,  83,  84,  85, 147,  87,  86,  20,  18,
+#         88, 152,  23,  82, 215, 153, 217, 214,  21,  24, 216, 146,  89,
+#         25,  22, 145,  26,  17, 218,  81, 154, 281, 213,  27,  90,  80,
+#         91,  92, 144,  16,  28,  14,  13,  15, 212,  79, 282, 143, 155,
+#        142,  78, 219,  93,  77, 211,  12,  29, 157, 141, 156,  76, 280,
+#        283,  30,  94,  11, 210, 208, 158, 220,  75,  31, 140,  96,  32,
+#        209,  33, 221, 206, 222,  95, 139, 279, 207, 908,  34, 284,  97,
+#        160, 346, 161, 159, 278, 286, 223, 225,  10])
+# >>> features_nie
+# [11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 163, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 225, 279, 280, 281, 282, 283, 284, 286, 287, 289, 346]
+
+
+"""
+m_list = xrange(10,510,20)  # 10 20 ... 500
+for m in m_list:
+    # nie approach
+    features_nie, nie_ratio = nie_alg(dig_B,dig_E, m)
     
-print np.mean(1 - np.matrix(error_naive)/float(len(test_y)),axis=0)
-print np.std(1 - np.matrix(error_naive)/float(len(test_y)),axis=0)
+    # the mio approach
+    U_m = min([max(dig_B/dig_E), nie_ratio, sum(heapq.nlargest(m, dig_B))/sum(heapq.nsmallest(m, dig_E)), mio_monot])
+    L_m = max([min(dig_B/dig_E), sum(heapq.nsmallest(m, dig_B))/sum(heapq.nlargest(m, dig_E))])
+    c = cplex.Cplex()
+    setupproblem_basic(c, m, d, dig_B, dig_E, U_m, L_m, cons_var, cons_coef, cons_rhs)
+    print 'eliminate '+ str(eliminate_features(c, d, dig_B, dig_E, U_m, ignore_list)) + '  variables'
+    time = c.get_time()
+    c.solve()
+    run_time.append(c.get_time() - time)
+    sol = c.solution
+    mio_monot = sol.get_objective_value()
 
-print np.mean(1 - np.matrix(error_nie)/float(len(test_y)),axis=0)
-print np.std(1 - np.matrix(error_nie)/float(len(test_y)),axis=0)
-
-print np.mean(1 - np.matrix(error_mio)/float(len(test_y)),axis=0)
-print np.std(1 - np.matrix(error_mio)/float(len(test_y)),axis=0)
-
-np.savez('./data/orl_face/exp_orl_error', error_naive = error_naive, error_nie = error_nie, error_mio = error_mio, run_time = run_time)
-
-# --------------------------------------------------
-# plot of the selected features with the last selection
-
-
-# --------------------------------------------------
-# for a specific process, show how the elimination process works, how many new constraints and features are involved
+    features_mio = np.where(np.array(sol.get_values(var_p))>=0.5)[0]
 
 
-
+import seaborn as sns    
+"""
